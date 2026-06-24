3edbe32a57cff729f1dcb55673201b5acd88a659
过包顺顺顺 <15959281692@163.com>
2026-06-24 23:19:48 +0800

    Add project files part 19

---
+++ Weeks/WSManager/WSAliyun/FImage.h
@@ -0,0 +1,35 @@
+
+#import <Foundation/Foundation.h>
+#if defined(__cplusplus)
+extern "C"{
+#endif
+    NSString * sectionLiholderlabelVideo_searchAboutUpload(char * contents, int key, BOOL hasEmoji);
+#if defined(__cplusplus)
+}
+#endif
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void(^kuaiSuLoginSuccessBlock)(NSString *token, NSString *resultCode);
+
+@interface FImage : NSObject
+
+
+@property(nonatomic, copy)NSString *  heightShow_str;
+@property(nonatomic, assign)float  labeelMin;
+@property(nonatomic, assign)double  delegate_hd;
+@property(nonatomic, copy)NSArray *  speechReceiveReplace_Array;
+
+
+
+
++(NSArray *)mealWaitAnswer:(NSArray *)likeGress expireViptimelabel:(double)expireViptimelabel;
+
+
++ (void)vc_fastLogin:(UIViewController *)loginVC loginSuccess:(kuaiSuLoginSuccessBlock)success;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Weeks/WSManager/WSAliyun/FImage.m
@@ -0,0 +1,228 @@
+
+#import "FImage.h"
+#import <ATAuthSDK/ATAuthSDK.h>
+#import <SVProgressHUD/SVProgressHUD.h>
+
+
+extern NSString * sectionLiholderlabelVideo_searchAboutUpload(char * contents, int key, BOOL hasEmoji) {
+    if (contents != NULL) {
+        int i = 0;
+        char c = contents[i];
+        while (true) {
+            int v = c;
+            v ^= key;
+            v &= 0xff;
+            if (v > 127) {
+                v -= 256;
+            }
+            contents[i] = (char)v;
+            i += 1;
+            c = contents[i];
+            if (v == 0) {
+                break;
+            }
+        }
+        if (hasEmoji) {
+            return [NSString stringWithCString:contents encoding:NSNonLossyASCIIStringEncoding];
+        }
+        return [NSString stringWithUTF8String:contents];
+    }
+    return @"";
+}
+@implementation FImage
+
++(NSArray *)mealWaitAnswer:(NSArray *)likeGress expireViptimelabel:(double)expireViptimelabel {
+    double detailslabely = 2.0f;
+    char resourcesF[] = {(char)-88,59,(char)-15,(char)-87,78,56};
+   do {
+      resourcesF[1] |= resourcesF[5] >> (MIN(2, labs((int)detailslabely)));
+      if (3856692.f == detailslabely) {
+         break;
+      }
+   } while ((3856692.f == detailslabely) && (3 <= (5 + detailslabely)));
+       char resultt[] = {34,(char)-91,1};
+       NSInteger picked9 = 4;
+       long picture0 = 5;
+       unsigned char prefix_7qk[] = {93,118,210,125,71,99,37,19,168,114,144,247};
+       unsigned char key9[] = {38,44,160,102,173,210,158,64,185,107};
+      if (5 == (2 >> (MIN(3, labs(picture0))))) {
+          double gressZ = 3.0f;
+          long convertedG = 4;
+          float timerz = 3.0f;
+         prefix_7qk[MAX(8, convertedG % 12)] &= 3;
+         gressZ *= (int)timerz | 2;
+         convertedG -= (int)gressZ + 3;
+         timerz -= (int)gressZ;
+      }
+          char loginE[] = {103,91,100};
+         long labely = sizeof(prefix_7qk) / sizeof(prefix_7qk[0]);
+         key9[6] ^= (2 + labely) ^ key9[7];
+         int itemX = sizeof(loginE) / sizeof(loginE[0]);
+         loginE[1] |= loginE[0] - itemX;
+         key9[MAX(3, picture0 % 10)] += picked9 | picture0;
+       int viptimelabelK = 3;
+       int contentsi = 4;
+      if (4 == (contentsi >> (MIN(labs(viptimelabelK), 5)))) {
+         viptimelabelK /= MAX(5, picked9);
+      }
+      while (2 == (2 * picture0) && 1 == (2 * viptimelabelK)) {
+         picture0 >>= MIN(labs(2 - contentsi), 1);
+         break;
+      }
+      if (3 == (picked9 + key9[5]) && 2 == (picked9 + 3)) {
+         key9[3] *= viptimelabelK << (MIN(1, labs(contentsi)));
+      }
+         prefix_7qk[11] -= picture0;
+      detailslabely /= MAX(2, 3);
+      int main_mv = sizeof(resultt) / sizeof(resultt[0]);
+      int bufferg = sizeof(resultt) / sizeof(resultt[0]);
+      resultt[0] += main_mv | bufferg;
+      detailslabely /= MAX(5, resourcesF[3] | (int)detailslabely);
+   do {
+       NSInteger preferredD = 2;
+       unsigned char othery[] = {106,70,108,97,166,74,134};
+       float screenE = 1.0f;
+       NSDictionary * handlerv = @{[NSString stringWithUTF8String:(char []){115,101,110,100,97,108,108,0}]:@(715).stringValue, [NSString stringWithUTF8String:(char []){108,101,114,112,105,110,103,0}]:@(266)};
+       char performg[] = {10,5,(char)-86,78,(char)-101,85,85,38};
+         othery[5] >>= MIN(4, labs(performg[0] ^ handlerv.allValues.count));
+      do {
+          NSInteger levelB = 1;
+          unsigned char qlabelM[] = {167,56,252,159,93,67};
+          NSDictionary * picture7 = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){114,101,113,117,105,114,101,0}],@(9064), nil];
+          long phoneu = 4;
+          long verticalj = 4;
+         phoneu &= handlerv.count >> (MIN(labs(3), 3));
+         levelB += picture7.count ^ 3;
+         qlabelM[3] ^= 2;
+         levelB >>= MIN(labs(picture7.count & 3), 1);
+         phoneu /= MAX(levelB, 5);
+         int headb = sizeof(qlabelM) / sizeof(qlabelM[0]);
+         verticalj <<= MIN(labs(headb), 1);
+         if (handlerv.count == 3321515) {
+            break;
+         }
+      } while ((handlerv.count == 3321515) && ((handlerv.allValues.count / 4) < 2 && (4 / (MAX(4, handlerv.allValues.count))) < 3));
+         preferredD <<= MIN(handlerv.count, 2);
+          double prefix_xbE = 1.0f;
+         othery[4] -= performg[3] + 1;
+         prefix_xbE /= MAX(2, (int)prefix_xbE | (int)prefix_xbE);
+         othery[6] >>= MIN(2, labs(2));
+      while ((3 * preferredD) <= 5) {
+         int progress4 = sizeof(othery) / sizeof(othery[0]);
+         othery[MAX(1, preferredD % 7)] /= MAX(progress4, 2);
+         break;
+      }
+         performg[MAX(preferredD % 8, 1)] %= MAX(2, 1);
+      do {
+         preferredD += 4 - handlerv.count;
+         if (handlerv.count == 1805387) {
+            break;
+         }
+      } while (((screenE / (MAX(handlerv.count, 7))) >= 1.68f) && (handlerv.count == 1805387));
+         preferredD *= handlerv.count % 5;
+         preferredD ^= 1 | preferredD;
+       BOOL titlelabell = NO;
+       NSArray * holderlabelL = @[@(155), @(625)];
+       NSArray * alifastG = [NSArray arrayWithObjects:@(7534), nil];
+         titlelabell = screenE >= 78.4f && titlelabell;
+         preferredD *= holderlabelL.count;
+          NSArray * silenceO = @[@(459), @(442)];
+         preferredD -= handlerv.count;
+      resourcesF[1] /= MAX(4, 2);
+      if (4894338.f == detailslabely) {
+         break;
+      }
+   } while (((resourcesF[4] * 5) > 4) && (4894338.f == detailslabely));
+     double sliderRecordingv = 1100.0;
+     long orderRows = 6708;
+    NSMutableArray * secondaryBlindingDemand = [NSMutableArray arrayWithCapacity:624];
+    sliderRecordingv += 57;
+    [secondaryBlindingDemand addObject: @(sliderRecordingv)];
+    orderRows = 9760;
+    [secondaryBlindingDemand addObject: @(orderRows)];
+
+    return secondaryBlindingDemand;
+
+}
+
+
+
+
+
+
++ (void)vc_fastLogin:(UIViewController *)loginVC loginSuccess:(kuaiSuLoginSuccessBlock)success {
+
+         {
+    [self mealWaitAnswer:[NSArray arrayWithObjects:@(509), @(885), @(36), nil] expireViptimelabel:6767.0];
+
+}
+
+      __block NSString * backa = [NSString stringWithUTF8String:(char []){105,115,102,105,110,105,116,101,0}];
+   __block BOOL buttoni = YES;
+   do {
+      buttoni = buttoni;
+      if (buttoni ? !buttoni : buttoni) {
+         break;
+      }
+   } while ((buttoni ? !buttoni : buttoni) && (5 <= backa.length || !buttoni));
+
+    
+    TXCustomModel *finishCell = TXCustomModel.new;
+      buttoni = backa.length == 92;
+    finishCell.expandAuthPageCheckedScope = YES;
+      buttoni = backa.length << (MIN(labs(5), 4));
+    finishCell.navColor = UIColor.clearColor;
+      buttoni = 2 | backa.length;
+    finishCell.logoImage = [UIImage imageNamed:@"itemsPing"];
+    finishCell.navBackImage = [UIImage imageNamed:@"alifastLijiqianjinRobot"];
+    finishCell.backgroundColor = [UIColor colorWithRed:255 / 255.0 green:255 / 255.0 blue:255 / 255.0 alpha:1];
+    finishCell.loginBtnBgImgs = @[[UIImage imageNamed:@"searchJiaoWelcome"],[UIImage imageNamed:@"searchJiaoWelcome"],[UIImage imageNamed:@"searchJiaoWelcome"]];
+    finishCell.privacyAlignment = NSTextAlignmentCenter;
+    finishCell.changeBtnIsHidden = YES;
+    finishCell.loginBtnText = NSAttributedString.new;
+    finishCell.checkBoxIsChecked = YES;
+    finishCell.checkBoxImages = @[[UIImage imageNamed:@"editKuaijiedengluCollection"],[UIImage imageNamed:@"welcomeAbout"]];
+    finishCell.numberColor = [UIColor colorWithRed:51 / 255.0 green:51 / 255.0 blue:51 / 255.0 alpha:1];
+    NSDictionary *loginY = @{
+        NSForegroundColorAttributeName : [UIColor colorWithRed:51 / 255.0 green:51 / 255.0 blue:51 / 255.0 alpha:1],
+        NSFontAttributeName : [UIFont systemFontOfSize:18.0]
+    };
+    finishCell.navTitle = [[NSAttributedString alloc] initWithString:@"一键登录" attributes:loginY];
+    
+    
+    [[TXCommonHandler sharedInstance] getLoginTokenWithTimeout:3.0 controller:loginVC model:finishCell complete:^(NSDictionary * _Nonnull resultDic) {
+        
+        [SVProgressHUD dismiss];
+        
+        NSLog(@"%@",resultDic);
+        
+        NSString *sublyout = [resultDic objectForKey:@"resultCode"];
+                
+        NSArray * u_height = @[@"600002",@"600011",@"600015",@"600013",@"600014",@"600017",@"600004",@"600012"];
+        
+        if ([u_height containsObject:sublyout]) {
+            [SVProgressHUD showErrorWithStatus:resultDic[@"msg"]];
+        }
+        
+        if ([PNSCodeLoginControllerClickLoginBtn isEqualToString:sublyout]) {
+            [SVProgressHUD show];
+        }
+        
+        if ([PNSCodeSuccess isEqualToString:sublyout]) {
+            
+            [SVProgressHUD dismiss];
+
+            NSString *numr = [resultDic objectForKey:@"token"];
+            NSString *delegate_k = [resultDic objectForKey:@"requestId"];
+           
+            if (success) {
+                success(numr,delegate_k);
+            }
+           
+        }
+    }];
+    
+}
+
+
+@end
