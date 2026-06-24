e8e982dab66727a658bd1612f0498ab4efba29ca
过包顺顺顺 <15959281692@163.com>
2026-06-24 23:15:06 +0800

    Add project files part 1

---
+++ Weeks/AppDelegate.swift
@@ -0,0 +1,399 @@
+
+import Foundation
+
+import UIKit
+import IQKeyboardManager
+import Alamofire
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+var pickerOffset: Double = 0.0
+var againLayerQueue_arr: [Any]?
+private var systemDictMain_string: String?
+
+
+
+
+    var window: UIWindow?
+
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       var prefix_mcZ: String! = String(cString: [99,111,112,121,109,0], encoding: .utf8)!
+    var screenf: Bool = false
+    var recordingvG: String! = String(cString: [115,104,97,114,100,0], encoding: .utf8)!
+      prefix_mcZ.append("\(((screenf ? 3 : 1) | 3))")
+      screenf = 3 <= prefix_mcZ.count && 3 <= recordingvG.count
+   if !screenf {
+       var leftu: String! = String(cString: [104,97,100,111,119,115,0], encoding: .utf8)!
+       var speedH: String! = String(cString: [99,111,109,112,97,116,0], encoding: .utf8)!
+      if speedH != String(cString:[54,0], encoding: .utf8)! {
+          var pickerP: Bool = true
+          var liholderlabelC: String! = String(cString: [100,105,116,97,110,99,101,0], encoding: .utf8)!
+          var detailsJ: Bool = true
+         leftu.append("\(1 + speedH.count)")
+         pickerP = (81 > ((pickerP ? liholderlabelC.count : 81) | liholderlabelC.count))
+         detailsJ = liholderlabelC.count <= 28
+      }
+         leftu.append("\(leftu.count)")
+         leftu.append("\((leftu == (String(cString:[117,0], encoding: .utf8)!) ? leftu.count : speedH.count))")
+          var urlsg: String! = String(cString: [115,105,122,105,110,103,0], encoding: .utf8)!
+          var cacheH: Int = 3
+         speedH = "\(leftu.count + 2)"
+         urlsg.append("\(cacheH ^ urlsg.count)")
+         cacheH |= 1
+      if leftu.count <= 4 {
+         leftu.append("\(leftu.count)")
+      }
+          var recognizedI: String! = String(cString: [108,105,98,115,119,115,99,97,108,101,0], encoding: .utf8)!
+          var d_centerY: Int = 1
+          var detaillabelj: String! = String(cString: [112,97,114,97,108,108,97,120,0], encoding: .utf8)!
+         leftu = "\(detaillabelj.count)"
+         recognizedI.append("\(d_centerY >> (Swift.min(labs(1), 4)))")
+         d_centerY >>= Swift.min(3, labs((recognizedI == (String(cString:[105,0], encoding: .utf8)!) ? recognizedI.count : d_centerY)))
+         detaillabelj.append("\(recognizedI.count)")
+      screenf = (leftu.count % (Swift.max(3, speedH.count))) > 40
+   }
+       var closeB: String! = String(cString: [104,102,121,117,0], encoding: .utf8)!
+       var datasD: [Any]! = [489, 85, 260]
+       var aboutX: Double = 1.0
+      while (5 == (5 << (Swift.min(5, closeB.count))) && (closeB.count << (Swift.min(labs(5), 2))) == 2) {
+         closeB = "\(Int(aboutX) / (Swift.max(5, datasD.count)))"
+         break
+      }
+          var pressb: Int = 1
+         datasD.append(datasD.count)
+         pressb >>= Swift.min(2, labs(1))
+      if 4 <= (closeB.count % 5) {
+         closeB = "\(2 % (Swift.max(6, datasD.count)))"
+      }
+         datasD.append(1)
+       var shul: String! = String(cString: [108,111,111,112,0], encoding: .utf8)!
+       var searchr: String! = String(cString: [105,112,111,108,0], encoding: .utf8)!
+      if searchr.count > 4 || shul != String(cString:[116,0], encoding: .utf8)! {
+          var bodyv: String! = String(cString: [111,99,116,101,116,0], encoding: .utf8)!
+         searchr = "\(bodyv.count | 2)"
+      }
+      while (1 <= shul.count) {
+          var descriptH: String! = String(cString: [104,101,118,109,97,115,107,0], encoding: .utf8)!
+          var with_oq7: [String: Any]! = [String(cString: [112,101,99,101,110,116,0], encoding: .utf8)!:95, String(cString: [99,108,111,115,101,0], encoding: .utf8)!:430]
+          var audiot: String! = String(cString: [97,117,116,104,111,114,105,122,101,0], encoding: .utf8)!
+          var graphics1: String! = String(cString: [99,111,114,110,101,114,0], encoding: .utf8)!
+         datasD = [descriptH.count]
+         descriptH = "\(3 + graphics1.count)"
+         with_oq7[graphics1] = 3
+         audiot.append("\((graphics1 == (String(cString:[90,0], encoding: .utf8)!) ? with_oq7.values.count : graphics1.count))")
+         break
+      }
+       var not_v9: Int = 1
+      repeat {
+          var collection4: Int = 3
+          var choosei: Int = 2
+          var qlabels: [String: Any]! = [String(cString: [112,111,115,116,97,108,0], encoding: .utf8)!:785, String(cString: [98,108,111,99,107,100,115,112,0], encoding: .utf8)!:36]
+          var first5: String! = String(cString: [119,104,101,101,108,0], encoding: .utf8)!
+         searchr = "\(1)"
+         collection4 /= Swift.max(1, qlabels.count)
+         choosei ^= 1 * qlabels.count
+         first5.append("\(collection4)")
+         if 3896559 == searchr.count {
+            break
+         }
+      } while (3896559 == searchr.count) && (closeB == searchr)
+      prefix_mcZ.append("\(closeB.count)")
+   if prefix_mcZ.count == 5 {
+       var audioe: String! = String(cString: [112,105,99,116,121,112,101,0], encoding: .utf8)!
+       var bottom1: [String: Any]! = [String(cString: [98,101,115,116,0], encoding: .utf8)!:820, String(cString: [117,110,115,117,112,112,111,114,116,101,100,0], encoding: .utf8)!:469]
+      while (!audioe.hasSuffix("\(bottom1.keys.count)")) {
+         bottom1 = ["\(bottom1.values.count)": (audioe == (String(cString:[103,0], encoding: .utf8)!) ? bottom1.values.count : audioe.count)]
+         break
+      }
+       var relationB: String! = String(cString: [97,116,99,104,101,114,0], encoding: .utf8)!
+         audioe = "\(2)"
+         bottom1 = ["\(bottom1.count)": 3]
+          var labelu: Float = 1.0
+          var layoutK: Bool = false
+         bottom1 = ["\(bottom1.count)": ((String(cString:[115,0], encoding: .utf8)!) == audioe ? bottom1.keys.count : audioe.count)]
+         labelu /= Swift.max(Float(2 & Int(labelu)), 3)
+         layoutK = !layoutK
+       var yhlogog: Double = 0.0
+      screenf = audioe.count == 14 || !screenf
+   }
+   if prefix_mcZ.contains("\(screenf)") {
+      screenf = !screenf
+   }
+   if recordingvG == recordingvG {
+      recordingvG = "\(recordingvG.count | recordingvG.count)"
+   }
+      recordingvG.append("\(2)")
+
+    }
+
+@discardableResult
+ func fullPlayerExpandQueueComponentFlow(toolEmpty: Double, detailDown: Double, terminateSocket: Double) -> [String: Any]! {
+    var update_d7A: [Any]! = [String(cString: [99,120,100,97,116,97,0], encoding: .utf8)!]
+    var task5: [Any]! = [UILabel()]
+      task5.append(task5.count)
+      task5 = [1 + update_d7A.count]
+   for _ in 0 ..< 1 {
+      update_d7A = [task5.count]
+   }
+   if 4 > task5.count {
+      update_d7A.append(3)
+   }
+     let statuesSort: Double = 8831.0
+     var enabledGesture: Double = 3528.0
+     let aboutPhoto: Double = 7108.0
+    var rpcgenInitialize :[String: Any]! = [String: Any]()
+    rpcgenInitialize.updateValue(statuesSort, forKey:String(cString: [100,0], encoding: .utf8)!)
+    enabledGesture = 6060
+    rpcgenInitialize.updateValue(enabledGesture, forKey:String(cString: [90,0], encoding: .utf8)!)
+    rpcgenInitialize.updateValue(aboutPhoto, forKey:String(cString: [107,0], encoding: .utf8)!)
+
+    return rpcgenInitialize
+
+}
+
+
+
+
+
+    
+    func detectNetworkStatus()  {
+
+             fullPlayerExpandQueueComponentFlow(toolEmpty:3118.0, detailDown:9924.0, terminateSocket:6598.0)
+
+
+       var keywordsO: String! = String(cString: [110,111,115,105,109,100,0], encoding: .utf8)!
+    var fileu: [String: Any]! = [String(cString: [99,97,116,101,103,111,114,105,115,101,100,0], encoding: .utf8)!:2293]
+       var graphicsI: Double = 1.0
+       var findS: Double = 4.0
+       var alabelq: [String: Any]! = [String(cString: [112,117,114,112,111,115,101,0], encoding: .utf8)!:String(cString: [115,119,102,112,108,97,121,101,114,0], encoding: .utf8)!, String(cString: [109,105,99,114,111,100,118,100,0], encoding: .utf8)!:String(cString: [112,117,115,104,105,110,103,0], encoding: .utf8)!]
+      while (!alabelq.values.contains { $0 as? Double == graphicsI }) {
+         alabelq = ["\(alabelq.count)": 2]
+         break
+      }
+      if (alabelq.values.count % (Swift.max(5, 8))) < 1 {
+          var arrayD: Int = 1
+         alabelq = ["\(graphicsI)": Int(graphicsI) + 3]
+         arrayD >>= Swift.min(4, labs(3 - arrayD))
+      }
+      repeat {
+         findS -= Double(Int(graphicsI) * 1)
+         if 898191.0 == findS {
+            break
+         }
+      } while (898191.0 == findS) && (3.2 < findS)
+          var ylabel5: [Any]! = [391, 105]
+         alabelq["\(findS)"] = Int(graphicsI)
+         ylabel5 = [3 / (Swift.max(7, ylabel5.count))]
+      for _ in 0 ..< 3 {
+          var indices4: Double = 1.0
+          var popupY: Float = 2.0
+          var insertF: String! = String(cString: [112,97,114,97,109,101,116,114,105,122,101,100,0], encoding: .utf8)!
+         alabelq = ["\(indices4)": 2]
+         indices4 /= Swift.max(Double(insertF.count), 5)
+         popupY += Float(1 & Int(popupY))
+         insertF.append("\(1 << (Swift.min(5, labs(Int(popupY)))))")
+      }
+       var speed5: Double = 0.0
+       var navgationN: Double = 0.0
+          var voice5: Double = 3.0
+          var screenw: Double = 5.0
+         graphicsI -= Double(1)
+         voice5 /= Swift.max(Double(Int(screenw)), 3)
+         screenw -= Double(Int(voice5))
+          var did7: String! = String(cString: [114,103,98,116,111,0], encoding: .utf8)!
+          var numberl: String! = String(cString: [105,104,100,114,0], encoding: .utf8)!
+         speed5 -= Double(Int(graphicsI) + 3)
+         did7 = "\(1 ^ did7.count)"
+         numberl = "\(did7.count)"
+      if 4.19 > (1.45 + navgationN) {
+         navgationN += Double(Int(speed5))
+      }
+      keywordsO = "\(Int(graphicsI))"
+
+   repeat {
+      fileu = ["\(fileu.keys.count)": keywordsO.count]
+      if 2800672 == fileu.count {
+         break
+      }
+   } while (3 <= (4 + keywordsO.count)) && (2800672 == fileu.count)
+        let other: NetworkReachabilityManager?
+        other = NetworkReachabilityManager.default
+        other?.startListening(onUpdatePerforming: { _ in })
+  
+   while ((4 << (Swift.min(5, keywordsO.count))) >= 1) {
+       var networkH: String! = String(cString: [115,104,97,114,105,110,103,0], encoding: .utf8)!
+       var dictu: Double = 0.0
+       var lishi3: String! = String(cString: [116,105,109,101,0], encoding: .utf8)!
+       var like6: String! = String(cString: [111,117,116,98,111,117,110,100,0], encoding: .utf8)!
+         lishi3 = "\(Int(dictu) + 1)"
+       var rateh: String! = String(cString: [115,121,110,99,104,114,111,110,105,122,101,0], encoding: .utf8)!
+      repeat {
+         networkH = "\(networkH.count | Int(dictu))"
+         if networkH == (String(cString:[117,108,57,54,116,52,103,116,116,107,0], encoding: .utf8)!) {
+            break
+         }
+      } while ((dictu / (Swift.max(4.98, 2))) >= 5.55) && (networkH == (String(cString:[117,108,57,54,116,52,103,116,116,107,0], encoding: .utf8)!))
+      while (like6.hasPrefix(rateh)) {
+          var bufferT: String! = String(cString: [100,101,113,117,97,110,116,0], encoding: .utf8)!
+          var rmbk: String! = String(cString: [105,110,116,101,114,110,0], encoding: .utf8)!
+          var userdatat: Double = 2.0
+          var settingT: [Any]! = [927, 20]
+         rateh.append("\(Int(dictu) | networkH.count)")
+         bufferT.append("\(Int(userdatat))")
+         rmbk = "\(Int(userdatat))"
+         settingT.append(((String(cString:[76,0], encoding: .utf8)!) == rmbk ? settingT.count : rmbk.count))
+         break
+      }
+          var elevt0: String! = String(cString: [115,101,112,97,114,97,116,111,114,0], encoding: .utf8)!
+         networkH = "\(rateh.count << (Swift.min(labs(3), 2)))"
+         elevt0.append("\((elevt0 == (String(cString:[73,0], encoding: .utf8)!) ? elevt0.count : elevt0.count))")
+         networkH.append("\(like6.count)")
+         dictu -= Double(3 - networkH.count)
+      if rateh.hasSuffix(like6) {
+          var bufferT_: [String: Any]! = [String(cString: [99,101,108,101,98,114,97,116,101,0], encoding: .utf8)!:115, String(cString: [109,105,100,110,105,103,104,116,0], encoding: .utf8)!:839]
+         like6.append("\(bufferT_.values.count)")
+      }
+         like6 = "\(rateh.count | Int(dictu))"
+      while (rateh == String(cString:[56,0], encoding: .utf8)!) {
+         like6.append("\(Int(dictu) ^ 2)")
+         break
+      }
+       var gif4: String! = String(cString: [109,97,105,110,0], encoding: .utf8)!
+      while (rateh == String(cString:[69,0], encoding: .utf8)!) {
+         lishi3.append("\(1)")
+         break
+      }
+      fileu[networkH] = lishi3.count + 1
+      break
+   }
+   if keywordsO.hasSuffix("\(fileu.values.count)") {
+      keywordsO = "\(3 >> (Swift.min(4, keywordsO.count)))"
+   }
+    }
+
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       var gifg: String! = String(cString: [100,99,97,109,97,116,104,0], encoding: .utf8)!
+    var speedb: Int = 0
+    var paint2: String! = String(cString: [98,105,119,103,116,0], encoding: .utf8)!
+       var enabledy: Double = 2.0
+       var chatJ: String! = String(cString: [109,111,118,101,110,99,99,101,110,99,0], encoding: .utf8)!
+       var meal3: [String: Any]! = [String(cString: [117,114,108,0], encoding: .utf8)!:String(cString: [101,102,102,101,99,116,105,118,101,108,121,0], encoding: .utf8)!, String(cString: [97,112,112,101,110,100,101,100,0], encoding: .utf8)!:String(cString: [100,101,99,111,109,112,114,101,115,115,0], encoding: .utf8)!, String(cString: [115,121,115,99,116,108,115,0], encoding: .utf8)!:String(cString: [115,101,116,102,105,101,108,100,0], encoding: .utf8)!]
+         enabledy -= Double(Int(enabledy))
+         enabledy *= Double(meal3.count)
+      repeat {
+          var didj: [Any]! = [241, 907]
+          var removeA: String! = String(cString: [112,108,117,114,97,108,105,122,101,100,0], encoding: .utf8)!
+         enabledy *= Double(meal3.values.count)
+         didj.append(removeA.count)
+         removeA.append("\(didj.count - 3)")
+         if enabledy == 3317636.0 {
+            break
+         }
+      } while (enabledy == 3317636.0) && ((5.17 * enabledy) > 2.41)
+          var detailslabel6: [String: Any]! = [String(cString: [99,104,97,114,108,101,110,0], encoding: .utf8)!:String(cString: [115,101,103,100,97,116,97,0], encoding: .utf8)!, String(cString: [119,97,114,110,105,110,103,115,0], encoding: .utf8)!:String(cString: [115,101,99,116,111,114,0], encoding: .utf8)!]
+          var speechL: String! = String(cString: [115,111,110,105,99,0], encoding: .utf8)!
+          var replacek: Double = 3.0
+         enabledy *= Double(speechL.count ^ detailslabel6.count)
+         detailslabel6 = ["\(replacek)": Int(replacek)]
+         speechL.append("\(Int(replacek))")
+          var addE: Float = 4.0
+         meal3["\(enabledy)"] = 2 ^ chatJ.count
+         addE += Float(Int(addE))
+         meal3["\(enabledy)"] = 2
+         chatJ.append("\(meal3.keys.count)")
+      for _ in 0 ..< 1 {
+          var rawing4: Bool = true
+          var notificationK: String! = String(cString: [97,99,99,101,115,115,111,114,0], encoding: .utf8)!
+         meal3[chatJ] = Int(enabledy)
+         rawing4 = !rawing4
+         notificationK.append("\((3 - (rawing4 ? 2 : 5)))")
+      }
+       var headersk: String! = String(cString: [97,117,116,111,108,111,99,107,0], encoding: .utf8)!
+       var browserH: String! = String(cString: [99,111,109,112,114,101,115,115,111,114,0], encoding: .utf8)!
+      gifg = "\(paint2.count)"
+
+      speedb ^= speedb / (Swift.max(7, gifg.count))
+        
+        detectNetworkStatus()
+       var alabelp: Double = 5.0
+       var scale4: [String: Any]! = [String(cString: [101,97,114,108,105,101,114,0], encoding: .utf8)!:18, String(cString: [112,97,115,115,116,104,114,111,117,103,104,0], encoding: .utf8)!:34, String(cString: [100,101,109,117,120,0], encoding: .utf8)!:983]
+       var hasI: String! = String(cString: [115,108,105,100,101,0], encoding: .utf8)!
+      repeat {
+          var phone8: Bool = true
+          var userl: Int = 0
+         hasI.append("\(hasI.count)")
+         phone8 = !phone8
+         userl *= 3 | userl
+         if hasI == (String(cString:[55,113,118,0], encoding: .utf8)!) {
+            break
+         }
+      } while (hasI == (String(cString:[55,113,118,0], encoding: .utf8)!)) && (1.35 == (Double(hasI.count) + alabelp) && 3 == (5 + Int(alabelp)))
+          var numberlabelu: String! = String(cString: [117,99,104,97,114,0], encoding: .utf8)!
+         hasI = "\(2 + hasI.count)"
+         numberlabelu = "\(numberlabelu.count - numberlabelu.count)"
+          var addressT: String! = String(cString: [121,95,49,54,0], encoding: .utf8)!
+          var networkq: String! = String(cString: [97,99,116,105,118,97,116,101,100,0], encoding: .utf8)!
+          var nnew_66B: [Any]! = [String(cString: [112,108,97,110,97,114,116,111,117,121,118,121,0], encoding: .utf8)!, String(cString: [109,111,99,107,101,100,0], encoding: .utf8)!]
+         scale4["\(addressT)"] = 2 + addressT.count
+         networkq.append("\(networkq.count & nnew_66B.count)")
+         nnew_66B.append(networkq.count)
+         hasI.append("\(2 * scale4.keys.count)")
+         scale4 = ["\(scale4.count)": hasI.count & 1]
+         alabelp *= Double(hasI.count)
+       var markK: String! = String(cString: [116,104,101,105,114,0], encoding: .utf8)!
+      repeat {
+         alabelp -= Double(hasI.count << (Swift.min(labs(2), 3)))
+         if 4168104.0 == alabelp {
+            break
+         }
+      } while (markK.count < Int(alabelp)) && (4168104.0 == alabelp)
+         scale4 = ["\(scale4.count)": Int(alabelp)]
+      speedb /= Swift.max(3 * gifg.count, 5)
+        
+        let relationController = QWFLoginController()
+      speedb |= gifg.count
+        
+        
+        KeTookingManager.shareTool(getAccountNumberIdentifier()!)
+        IQKeyboardManager.shared().isEnabled = true
+      gifg.append("\(paint2.count)")
+        IQKeyboardManager.shared().shouldResignOnTouchOutside = true
+      paint2 = "\((gifg == (String(cString:[95,0], encoding: .utf8)!) ? speedb : gifg.count))"
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+   
+        
+        if let first = UserDefaults.standard.object(forKey: "first") as? Int {
+            if first == 1 {
+                
+                if UserDefaults.standard.object(forKey: "AccountToken") == nil {
+                    window?.rootViewController = UINavigationController(rootViewController: APChatFirstController())
+                }
+                
+                else {
+                    window?.rootViewController = SCHistoryController()
+                }
+                
+            }
+            else {
+                window?.rootViewController = UINavigationController(rootViewController: DBufferHeaderController())
+            }
+        }
+        else {
+            window?.rootViewController = UINavigationController(rootViewController: DBufferHeaderController())
+        }
+        
+        window?.makeKeyAndVisible()
+        
+        TXCommonHandler.sharedInstance().setAuthSDKInfo("dIjf977gwa3IxnRgJa/n4kUBnvsszIzVwEnT6kgDcZ6k9poHpPjTpOcKazk3hP5Y2wWM/ANDQZ2UPRurW9mhfJihcrtLhvk2ZdnbGzQjPsXnPRUf/PpIptN1pG2MAa1E9Dgm4ZKQqrbWuKtVzCTHaZ0zIJnyphaAI6Ik3gNZIdOhwPr4v+bpZ3eEEvX+j7ABHn4bRIkU+eOkaVZhyJKtEhQy+QiwI1jg+o4j+cak6PGtAZWz/0MV9FsKfIyb/Hf3") { resultDic in
+
+            print("设置秘钥结果：\(resultDic)")
+        }
+        
+        return true
+    }
+
+}
+
